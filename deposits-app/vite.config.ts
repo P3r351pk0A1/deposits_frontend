57feeb373481051c517fe176d4d059b8073b57c2
Alexandr <peresipko_s2005@mail.ru>
2024-12-02 04:51:54 +0300

    add gh

--- deposits-app/vite.config.ts
+++ deposits-app/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/deposits_frontend",
   server: {
     host: true, // изначально добавил для доступа по локальной сети
     port: 3000,
